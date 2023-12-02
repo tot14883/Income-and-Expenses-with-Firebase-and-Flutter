1a5f1d3c7bbeb09fab7a67311f1c934a32b88ddd
Napat Srichantapong <>
2023-12-03 00:25:22 +0700

    add return null value

--- lib/features/income_expenses/usecase/read_income_expenses_usecase.dart
+++ lib/features/income_expenses/usecase/read_income_expenses_usecase.dart
@@ -14,39 +14,43 @@ class ReadIncomeExpensesUseCase extends UseCase<String, MyAccountResponse> {
 
   @override
   Future<MyAccountResponse> exec(String request) async {
-    final collect = await firebaseStoreDatabase
-        .collection('users')
-        .doc(UserAndPass.token)
-        .get()
-        .then((value) => value.data() as Map<String, dynamic>);
+    try {
+      final collect = await firebaseStoreDatabase
+          .collection('users')
+          .doc(UserAndPass.token)
+          .get()
+          .then((value) => value.data() as Map<String, dynamic>);
 
-    final incomeExpensesListCollect = await firebaseStoreDatabase
-        .collection('users')
-        .doc(UserAndPass.token)
-        .collection('income_expenses_list')
-        .orderBy('created_at')
-        .get()
-        .then((value) => value.docs);
+      final incomeExpensesListCollect = await firebaseStoreDatabase
+          .collection('users')
+          .doc(UserAndPass.token)
+          .collection('income_expenses_list')
+          .orderBy('created_at')
+          .get()
+          .then((value) => value.docs);
 
-    final incomeExpensesMoney = collect['income_expenses_money'];
-    List<MyAccountDetailResponse> incomeExpensesList = [];
+      final incomeExpensesMoney = collect['income_expenses_money'];
+      List<MyAccountDetailResponse> incomeExpensesList = [];
 
-    for (final item in incomeExpensesListCollect.reversed) {
-      final _data = item.data();
-      incomeExpensesList.add(
-        MyAccountDetailResponse(
-          id: _data['id'],
-          detail: _data['detail'],
-          money: _data['money'],
-          dateTime: _data['dateTime'],
-          type: TypeIncomeExpenses.values.byName(_data['type']),
-        ),
+      for (final item in incomeExpensesListCollect.reversed) {
+        final _data = item.data();
+        incomeExpensesList.add(
+          MyAccountDetailResponse(
+            id: _data['id'],
+            detail: _data['detail'],
+            money: _data['money'],
+            dateTime: _data['dateTime'],
+            type: TypeIncomeExpenses.values.byName(_data['type']),
+          ),
+        );
+      }
+
+      return MyAccountResponse(
+        incomeExpenseMoney: incomeExpensesMoney,
+        myAccountDetail: incomeExpensesList,
       );
+    } catch (e) {
+      return const MyAccountResponse();
     }
-
-    return MyAccountResponse(
-      incomeExpenseMoney: incomeExpensesMoney,
-      myAccountDetail: incomeExpensesList,
-    );
   }
 }
